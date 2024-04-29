dbd86e05865687328f23466820c197b6428cb97b
marksikaundi <emmanuelmark117@gmail.com>
2024-04-30 01:13:55 +0200

    Refactor layout component and update Tailwind configuration

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Ora</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
