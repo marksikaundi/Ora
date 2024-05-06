9c93edc2bc19d185f886f2525699b32cca990fec
marksikaundi <emmanuelmark117@gmail.com>
2024-05-06 20:41:08 +0200

    Remove unused authentication and tab components

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,9 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import { Slot, SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
@@ -22,15 +24,28 @@ const RootLayout = () => {
   useEffect(() => {
     if (error) throw error;
 
-    if (fontsLoaded) SplashScreen.hideAsync();
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
   }, [fontsLoaded, error]);
 
-  if (!fontsLoaded && !error) return null;
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
   );
 };
 
